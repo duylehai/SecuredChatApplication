a6ebf4fbc073c743ae9ece5b6cf72f06bfce09cd
duylehai <lehaiduy2112@gmail.com>
2023-07-01 21:25:08 +0700

    first commit

---
+++ src/main/java/com/FinalProject/SecuredChaApplication/SecuredChaApplication.java
@@ -0,0 +1,13 @@
+package com.FinalProject.SecuredChaApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredChaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredChaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FinalProject/SecuredChaApplication/SecuredChaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinalProject.SecuredChaApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredChaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
