9ff73dfb31831f45c2d674f3dca1a2ce8cc9cfe5
Lê Hải Duy <63355568+duylehai@users.noreply.github.com>
2023-07-04 15:02:58 +0700

    Create Cryptography classes

---
+++ src/main/java/com/FinalProject/SecuredChatApplication/Cryptography/AES.java
@@ -0,0 +1,51 @@
+package com.FinalProject.SecuredChatApplication.Cryptography;
+
+import java.security.*;
+import java.security.spec.*;
+import java.util.*;
+
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
+public class AES {
+	public static SecretKey getKeyFromPassword(String password, String salt)
+		    throws NoSuchAlgorithmException, InvalidKeySpecException {
+		    
+		    SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+		    KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+		    SecretKey secret = new SecretKeySpec(factory.generateSecret(spec)
+		        .getEncoded(), "AES");
+		    return secret;
+		}
+	
+	public static IvParameterSpec generateIv() {
+	    byte[] iv = new byte[16];
+	    new SecureRandom().nextBytes(iv);
+	    return new IvParameterSpec(iv);
+	}
+	
+	public static String encrypt(String input, SecretKey key,
+		    IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+		    InvalidAlgorithmParameterException, InvalidKeyException,
+		    BadPaddingException, IllegalBlockSizeException {
+		    
+		    Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		    cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+		    byte[] cipherText = cipher.doFinal(input.getBytes());
+		    return Base64.getEncoder()
+		        .encodeToString(cipherText);
+		}
+	
+	public static String decrypt(String cipherText, SecretKey key,
+		    IvParameterSpec iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+		    InvalidAlgorithmParameterException, InvalidKeyException,
+		    BadPaddingException, IllegalBlockSizeException {
+		    
+		    Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		    cipher.init(Cipher.DECRYPT_MODE, key, iv);
+		    byte[] plainText = cipher.doFinal(Base64.getDecoder()
+		        .decode(cipherText));
+		    return new String(plainText);
+		}
+	
+}
