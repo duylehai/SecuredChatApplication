8427f2b4dca0dfc519a1e5002d8faa7fe45f4f98
Lê Hải Duy <63355568+duylehai@users.noreply.github.com>
2023-07-07 11:27:50 +0700

    re-implement controller

--- src/main/java/com/FinalProject/SecuredChatApplication/RESTController.java
+++ src/main/java/com/FinalProject/SecuredChatApplication/RESTController.java
@@ -1,75 +1,93 @@
 package com.FinalProject.SecuredChatApplication;
 
-import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
-import org.springframework.scheduling.support.SimpleTriggerContext;
-import org.springframework.web.bind.annotation.*;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.FinalProject.SecuredChatApplication.Cryptography.AES;
+import com.FinalProject.SecuredChatApplication.Cryptography.RSA;
 import com.FinalProject.SecuredChatApplication.JSONAdapter.JSONAdapter;
+import com.FinalProject.SecuredChatApplication.Model.User;
+import com.FinalProject.SecuredChatApplication.Service.UserService;
 import com.FinalProject.SecuredChatApplication.SocketServer.WebSocketEventListener;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+
+import javafx.util.Pair;
 
 @RestController
 public class RESTController {
-	
-	private final String SUCCESS_CODE = "Sir yes Sir!";
-	
-	private boolean checkIdentity(String data) {
-		if (data == null || !data.equals(SUCCESS_CODE)) {
-				return false;
-		}
-		return true;
-	}
+	private final UserService userService = ContextProvider.getApplicationContext().getBean(UserService.class);
 	
 	@GetMapping("/greeting")
 	public String greeting() {
 		return  "hello user! test api";
 	}
 	
-	@GetMapping("/getPublicKey")
-	public String getPublicKey() {
-		return "public key"; 					// SQL this public key of authenticate server
+	@GetMapping("/getUserPublicKey/{username}")
+	public String getPublicKey(@PathVariable("username") String username) {
+		return userService.getUserPublicKey(username);
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(@RequestBody String requestData) {
-		String privateKey = "private key"; 		// SQL this private key of authenticate server
-		
-		// decrypt stuff me do this
-		String decryptedData = new String(requestData);
-		
-		Map<String, String> myMap = JSONAdapter.deserialize(decryptedData);
-	
-		
+	public String login(@RequestBody String requestData) { 
+		Map<String, String> myMap = JSONAdapter.deserialize(requestData);
 		String pw = myMap.get("password");
-		String EPW = "EPW"; 					// SQL this 
-		String decryptedEPW = "DEPW";			// crypto stuff me do this
-		if (decryptedEPW.equals("sir yes sir!"))
-		{
-			String sessionKey = "sessionkey";
-			String IV = "IV";
-			String output = "{\"sessionKey\": \"" + sessionKey + "\"," + "\"IV\":\"" + IV + "\"}";
-			String decryptedOutput = new String(output); // ctypto stuff again
-			return decryptedOutput;
+		String username = myMap.get("username");
+		
+		try {	
+			User user = userService.isValidUser(username, pw);
+			
+			if (user == null)
+				return "Login Failed!";
+			
+			Map<String, String> respond = new HashMap<>();
+			respond.put("private key", user.getEncryptedPrivateKey());
+			respond.put("salt", user.getSalt());
+			respond.put("IV", user.getIV());
+			
+			// NOTE: serialize method is NOT IMPLEMETED.
+			return JSONAdapter.serialize(respond);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "login failed";
 		}
-		return "Login Failed!";
 	}
 	
 	@GetMapping("/register")
 	public String register(@RequestBody String requestData) {
-		String privateKey = "private key"; 		// SQL this private key of authenticate server
-		// decrypt stuff me do this -- WITH PRIVATE KEY
-		String decryptedData = new String(requestData);
-		Map<String, String> myMap = JSONAdapter.deserialize(decryptedData);
-		
-		
+		Map<String, String> myMap = JSONAdapter.deserialize(requestData);
 		String username = myMap.get("username");
 		String password = myMap.get("password");
-		password = new String("sir yes sir"); // encrypt this shit, store this to DB
 		
-		return "register success";
+		try {
+			Pair<String, String> keyPair = RSA.generatekeyPair();
+			String salt = randomSalt();
+			
+			SecretKey aesKey = AES.getKeyFromPassword(password, salt);
+			IvParameterSpec aesIv = AES.generateIv();
+			String encryptedPrivateKey = AES.encrypt(keyPair.getKey(), aesKey, aesIv);
+			String encryptedDummy = AES.encrypt("dummy dummy dummy", aesKey, aesIv);
+			String aesIVString = Base64.getEncoder().encodeToString(aesIv.getIV());
+			User user = new User(username, encryptedDummy, keyPair.getValue(), encryptedPrivateKey, salt, aesIVString);
+			
+			userService.addUser(user);
+			
+			return "register success";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "register failed";
+		}
 	}
 	
 	@GetMapping("/isActive/{username}")
@@ -87,4 +105,14 @@ public class RESTController {
 		}
 		return "false";
 	}
+	
+	private String randomSalt() {
+		byte[] byteSalt = new byte[10];
+		new Random().nextBytes(byteSalt);
+		String salt = new String(byteSalt, StandardCharsets.UTF_8);
+		
+		return salt;
+	}
+	
+	
 }
--- src/main/java/com/FinalProject/SecuredChatApplication/SocketServer/ChatController.java
+++ src/main/java/com/FinalProject/SecuredChatApplication/SocketServer/ChatController.java
@@ -2,16 +2,9 @@ package com.FinalProject.SecuredChatApplication.SocketServer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.Map;
 
 @Controller
 public class ChatController {
@@ -21,17 +14,10 @@ public class ChatController {
     
     @MessageMapping("/chat/{owner}/{username}")
     public String sendMsgToUser(final String message,@PathVariable String owner, @PathVariable String username) throws Exception {
+    	if (! WebSocketEventListener.isActive(owner))
+    		return "sender not active";
     	
-    	// 
-    	String sessionKeySender = "<session key>"; //query session key of owner
-    	String sessionkeyOwner =  "<session key>";
-    	// get Initial vector for AES implement soon
-    	String decryptedMessage = new String(message); // decrypt with owner session key and initial vector
-    	String encryptedMessage = new String(message); // encrypt with receiver session key and initial vector
-    	
-    	// TODO: SQL THIS CHAT
-    	
-    	simpMessagingTemplate.convertAndSendToUser(username, "/queue/messages", encryptedMessage);
+    	simpMessagingTemplate.convertAndSendToUser(username, "/queue/messages", message);
     	return "done";
     }
 }
