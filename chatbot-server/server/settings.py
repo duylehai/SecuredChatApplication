a3e1218c1298f903812c22512c6f1410b84a6ca1
hungt1 <nguyenvanhung.brvt.2002@gmail.com>
2023-07-10 23:42:13 +0700

    add cors setting

--- chatbot-server/server/settings.py
+++ chatbot-server/server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",  # Django REST framework
     "channels",  # Django Channels
     "api",  # API app
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ASGI_APPLICATION = "server.asgi.application"
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -134,3 +136,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CORS_ALLOWED_ORIGINS = [
+#     'https://0480-2a09-bac5-d469-18c8-00-278-74.ngrok-free.app',
+#     'http://localhost:3000',
+# ]
