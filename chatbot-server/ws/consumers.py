49c42c4c69413b4d17b1d1e6a8a112445b51585d
duylehai <lehaiduy2112@gmail.com>
2023-07-12 09:33:42 +0700

    remove socket code when successfully loged in

--- chatbot-server/ws/consumers.py
+++ chatbot-server/ws/consumers.py
@@ -21,7 +21,7 @@ class ChatConsumer(WebsocketConsumer):
         if str(self.user) not in socketUser or str(params[b'socket_code'][0].decode()) != socketUser.get(str(self.user)):
             self.close()
             
-        socketUser.pop(str(self.user))
+        socketUser.pop(str(self.user)) 
 
         self.accept()
         self.group_name = f'chat_{self.user}'
